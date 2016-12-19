Adarsh-MR/frameworks_base

Make QS columns count configurable
And add infrastructure to add more settings and
distribute them to QS panel

PS4
we must explicitly hide the columns submenu

*DU Edits*

- Remove QSCustomizer menu item/qs changer
- Make edits to use seekbar in settings

Screenshots
https://i.imgur.com/tyH2EZfg.png
https://i.imgur.com/MrUc0aTg.png
https://i.imgur.com/oQIrWizg.png
https://i.imgur.com/jsVZ7kTg.png
https://i.imgur.com/hVIZQiGg.png
https://i.imgur.com/Rfa5DSxg.png

packages/SystemUI/src/com/android/systemui/statusbar/phone/BaseStatusBarHeader.java
@@ -46,4 +46,5 @@ public abstract class BaseStatusBarHeader extends RelativeLayout implements
     public abstract void setNextAlarmController(NextAlarmController nextAlarmController);
     public abstract void setUserInfoController(UserInfoController userInfoController);
     public abstract void setCallback(Callback qsPanelCallback);
+    public abstract void updateSettings(); 
 }

Adarsh-MR/platform_frameworks_base

packages/SystemUI/src/com/android/systemui/statusbar/phone/BaseStatusBarHeader.java
@@ -46,4 +46,5 @@ public abstract class BaseStatusBarHeader extends RelativeLayout implements
     public abstract void setNextAlarmController(NextAlarmController nextAlarmController);
     public abstract void setUserInfoController(UserInfoController userInfoController);
     public abstract void setCallback(Callback qsPanelCallback);
+    public abstract void update(); 
 }